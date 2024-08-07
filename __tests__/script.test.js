a34f26c00d5433613e071d2d70e711fdfe21d920
mohan <mohan14242@gmail.com>
2024-08-07 21:24:24 +0530

    mohan

---
+++ __tests__/script.test.js
@@ -0,0 +1,33 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const fs = require('fs');
+const path = require('path');
+
+describe('Basic JavaScript App', () => {
+    let document;
+
+    beforeAll(() => {
+        // Load the HTML document
+        document = new DOMParser().parseFromString(
+            fs.readFileSync(path.resolve(__dirname, '../src/src/index.html'), 'utf8'),
+            'text/html'
+        );
+
+        // Load and execute the script
+        require('../src/src/script')(document);
+    });
+
+    test('button click should show an alert', () => {
+        // Mock the alert function
+        global.alert = jest.fn();
+
+        // Get the button element and click it
+        const button = document.getElementById('myButton');
+        button.click();
+
+        // Check if alert was called
+        expect(global.alert).toHaveBeenCalledWith('Button clicked!');
+    });
+});
